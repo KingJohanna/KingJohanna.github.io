beac9cf3c913c24d9e71f3bac4769390e920fcfb
Adrian Murphy <adrian.murphy99@mail.com>
2023-02-24 15:51:52 +0100

    login or whatever

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { AngularFireModule } from "@angular/fire/compat";
+import { AngularFireAuthModule } from "@angular/fire/compat/auth";
+import { environment } from 'src/environments/environment';
+import { LogInComponent } from './components/log-in/log-in.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { AuthService } from 'src/app/shared/auth.service';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LogInComponent,
+    DashboardComponent
   ],
   imports: [
+    AngularFireModule.initializeApp(environment),
+    AngularFireAuthModule,
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/environments/environment.ts
@@ -0,0 +1,9 @@
+export const environment = {
+    apiKey: "AIzaSyCvOxLmq6syDGA5fJNHPs5VpdHJeZd1sd8",
+    authDomain: "edaf90-project-movie.firebaseapp.com",
+    projectId: "edaf90-project-movie",
+    storageBucket: "edaf90-project-movie.appspot.com",
+    messagingSenderId: "1048886304370",
+    appId: "1:1048886304370:web:0ca8ac346519860a192ceb",
+    measurementId: "G-DD5Q9N5VGM"
+};
